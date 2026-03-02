488b40c900ccc7337294b94646aafc0d927bf7b3
Khomyakov, Vladimir <inmatrix.ru@gmail.com>
2026-03-02 20:40:14 +0300

    Add files via upload

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,9 @@
+import CPLGovernorDashboard from "./cpl_governor_dashboard.jsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: 16 }}>
+      <CPLGovernorDashboard />
+    </div>
+  );
+}
